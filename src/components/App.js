fa139f1c4f0b9e72ccc96bfd227097921d5d90f8
Tushar kumar <narutoboi41@gmail.com>
2026-04-20 13:41:47 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Tours from "./Tours";
+import Tour from "./Tour";
+import '../styles/App.css'
 
 const App = () => {
     return(
       <main id="main">
+        <Tours/>
+        {/* <Tour/> */}
       </main>
     )
 }
---
+++ src/components/Tours.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tour from "./Tour";
+
+const Tours = () => {
+
+    let [tour, setTour] = useState([])
+    let [load, setLoad] = useState(false)
+
+    function tourData() {
+        fetch('https://www.course-api.com/react-tours-project')
+            .then((res) => res.json())
+            .then((ans) => {
+                setTour(ans);
+                setLoad(true);
+                console.log(ans);
+            });
+    }
+    useEffect(() => {
+        tourData()
+    }, [])
+
+    if (!load) {
+        return <Loading />
+    }
+
+
+    return (
+        <div className="tours">
+            
+            {tour.map(value=>(
+                <Tour 
+                value = {value}
+                />
+            ))}
+        </div>
+    )
+}
+export default Tours
